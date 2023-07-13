7ee9be7b4f0af2fd4c12a79612cd0ac820ec589c
Remy Araya <remy@remyaraya.org>
2023-07-13 12:48:05 -0500

    Added - RG Tags

--- resource-groups.tf
+++ resource-groups.tf
@@ -1,9 +1,17 @@
 resource "azurerm_resource_group" "rg_dev01_eu2" {
   name     = var.dev_rg01_name
   location = "eastus2"
+
+   tags = {
+    environment = "development"
+  }
 }
 
 resource "azurerm_resource_group" "rg_prod01_eu2" {
   name     = var.prod_rg01_name
   location = "eastus2"
+
+   tags = {
+    environment = "production"
+  }
 }
