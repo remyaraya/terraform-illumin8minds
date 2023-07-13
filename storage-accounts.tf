738e9e82f5472bbc976e053aa82e9ac53c72c465
Remy Araya <remy@remyaraya.org>
2023-07-13 11:33:37 -0500

    Updated - storage account nested items

--- storage-accounts.tf
+++ storage-accounts.tf
@@ -4,7 +4,6 @@ resource "azurerm_storage_account" "dev-storage" {
   location                 = azurerm_resource_group.rg_dev_eu2.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
-  allow_nested_items_to_be_public = false
   
   tags = {
     environment = "dev"
